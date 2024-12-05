268f7dceab667cba03a5d125ab3acc6ba30d0ae2
Vitor andes <vitor.andes.santos04@gmail.com>
2024-12-05 17:56:42 -0400

    :sparkles: feat: -- add fetch OwnedGames and gameDetails

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ Fastify.get("/user", async (request, reply) => {
       `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${steamApiKey}&steamids=${steamApiId}&format=json`
     );
     const data = await response.json();
-    console.log(data);
 
     reply.send(data);
   } catch (error) {
@@ -34,10 +33,56 @@ Fastify.get("/friends", async (request, reply) => {
       `https://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=${steamApiKey}&steamid=${steamApiId}&relationship=friend`
     );
     const data = await response.json();
-    console.log(data);
 
     reply.send(data);
   } catch (error) {
+    reply.status(500).send({ error: "Failed to fetch data from API" });
+  }
+});
+
+Fastify.get("/games", async (request, reply) => {
+  try {
+    const response = await fetch(
+      `https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${steamApiKey}&steamid=${steamApiId}&format=json`
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch owned games");
+    }
+
+    const data = await response.json();
+
+    console.log("reponse games", data);
+
+    if (!data.response || !data.response.games) {
+      return reply.status(404).send({ error: "No games found" });
+    }
+
+    const gameIds = data.response.games.map((game) => game.appid);
+
+    const detailsPromises = gameIds.map(async (gameId) => {
+      const detailsResponse = await fetch(
+        `https://store.steampowered.com/api/appdetails?appids=${gameId}`
+      );
+
+      if (!detailsResponse.ok) {
+        throw new Error(`Failed to fetch details for gameId: ${gameId}`);
+      }
+
+      return detailsResponse.json();
+    });
+
+    const detailsData = await Promise.all(detailsPromises);
+
+    const combinedDetails = detailsData.reduce((acc, details) => {
+      const key = Object.keys(details)[0];
+      acc[key] = details[key];
+      return acc;
+    }, {});
+
+    reply.send(combinedDetails);
+  } catch (error) {
+    console.error(error);
     reply.status(500).send({ error: "Failed to fetch data from API" });
   }
 });
