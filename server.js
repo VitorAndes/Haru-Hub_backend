89ba6fdf156af819e2f6178a1b052c85ec647d39
Vitor Andes <vitor.andes.santos04@gmail.com>
2025-02-12 17:47:22 -0400

    :sparkles: adding new route for games played recently

--- server.js
+++ server.js
@@ -10,83 +10,125 @@ const steamApiId = process.env.STEAM_API_ID;
 const Fastify = fastify({ logger: true });
 
 await Fastify.register(cors, {
-  origin: "*",
-  methods: ["GET"],
+	origin: "*",
+	methods: ["GET"],
 });
 
 Fastify.get("/user", async (request, reply) => {
-  try {
-    const response = await fetch(
-      `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${steamApiKey}&steamids=${steamApiId}&format=json`
-    );
-    const data = await response.json();
+	try {
+		const response = await fetch(
+			`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${steamApiKey}&steamids=${steamApiId}&format=json`,
+		);
+		const data = await response.json();
 
-    reply.send(data);
-  } catch (error) {
-    reply.status(500).send({ error: "Failed to fetch data from API" });
-  }
+		reply.send(data);
+	} catch (error) {
+		reply.status(500).send({ error: "Failed to fetch data from API" });
+	}
 });
 
 Fastify.get("/friends", async (request, reply) => {
-  try {
-    const response = await fetch(
-      `https://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=${steamApiKey}&steamid=${steamApiId}&relationship=friend`
-    );
-    const data = await response.json();
+	try {
+		const response = await fetch(
+			`https://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=${steamApiKey}&steamid=${steamApiId}&relationship=friend`,
+		);
+		const data = await response.json();
 
-    reply.send(data);
-  } catch (error) {
-    reply.status(500).send({ error: "Failed to fetch data from API" });
-  }
+		reply.send(data);
+	} catch (error) {
+		reply.status(500).send({ error: "Failed to fetch data from API" });
+	}
 });
 
 Fastify.get("/games", async (request, reply) => {
-  try {
-    const response = await fetch(
-      `https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${steamApiKey}&steamid=${steamApiId}&format=json`
-    );
+	try {
+		const response = await fetch(
+			`https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${steamApiKey}&steamid=${steamApiId}&format=json`,
+		);
 
-    if (!response.ok) {
-      throw new Error("Failed to fetch owned games");
-    }
+		if (!response.ok) {
+			throw new Error("Failed to fetch owned games");
+		}
 
-    const data = await response.json();
+		const data = await response.json();
 
-    console.log("reponse games", data);
+		console.log("reponse games", data);
 
-    if (!data.response || !data.response.games) {
-      return reply.status(404).send({ error: "No games found" });
-    }
+		if (!data.response || !data.response.games) {
+			return reply.status(404).send({ error: "No games found" });
+		}
 
-    const gameIds = data.response.games.map((game) => game.appid);
+		const gameIds = data.response.games.map((game) => game.appid);
 
-    const detailsPromises = gameIds.map(async (gameId) => {
-      const detailsResponse = await fetch(
-        `https://store.steampowered.com/api/appdetails?appids=${gameId}`
-      );
+		const detailsPromises = gameIds.map(async (gameId) => {
+			const detailsResponse = await fetch(
+				`https://store.steampowered.com/api/appdetails?appids=${gameId}`,
+			);
 
-      if (!detailsResponse.ok) {
-        throw new Error(`Failed to fetch details for gameId: ${gameId}`);
-      }
+			if (!detailsResponse.ok) {
+				throw new Error(`Failed to fetch details for gameId: ${gameId}`);
+			}
 
-      return detailsResponse.json();
-    });
+			return detailsResponse.json();
+		});
 
-    const detailsData = await Promise.all(detailsPromises);
+		const detailsData = await Promise.all(detailsPromises);
 
-    const combinedDetails = detailsData.reduce((acc, details) => {
-      const key = Object.keys(details)[0];
-      acc[key] = details[key];
-      return acc;
-    }, {});
+		const combinedDetails = detailsData.reduce((acc, details) => {
+			const key = Object.keys(details)[0];
+			acc[key] = details[key];
+			return acc;
+		}, {});
 
-    reply.send(combinedDetails);
-  } catch (error) {
-    console.error(error);
-    reply.status(500).send({ error: "Failed to fetch data from API" });
-  }
+		reply.send(combinedDetails);
+	} catch (error) {
+		console.error(error);
+		reply.status(500).send({ error: "Failed to fetch data from API" });
+	}
+});
+
+Fastify.get("/recentlyPlayedGames", async (request, reply) => {
+	try {
+		const response = await fetch(
+			`https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${steamApiKey}&steamid=${steamApiId}&format=json`,
+		);
+
+		if (!response.ok) {
+			throw new Error("Failed to fetch RecentlyPlayedGames");
+		}
+
+		const data = await response.json();
+
+		const gameIds = data.response.games.map((game) => game.appid);
+
+		const detailsPromises = gameIds.map(async (gameId) => {
+			const detailsResponse = await fetch(
+				`https://store.steampowered.com/api/appdetails?appids=${gameId}`,
+			);
+
+			if (!detailsResponse.ok) {
+				throw new Error(`Failed to fetch details for gameId: ${gameId}`);
+			}
+
+			return detailsResponse.json();
+		});
+
+		const detailsData = await Promise.all(detailsPromises);
+
+		const combinedDetails = detailsData.reduce((acc, details) => {
+			const key = Object.keys(details)[0];
+			acc[key] = details[key];
+			return acc;
+		}, {});
+
+		reply.send(combinedDetails);
+		reply.header("Access-Control-Allow-Origin", "*");
+	} catch (error) {
+		console.error(error);
+		reply.status(500).send({ error: "Failed to fetch data from API" });
+	}
 });
 
 Fastify.listen({
-  port: port,
+	port: port,
 });
